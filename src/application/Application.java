5a5e7589b8a30060e6f1246559812bdeb7424fc1
Paul Peng <paulpeng@gmail.com>
2026-06-02 09:14:49 -0400

    Application

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,4 +1,43 @@
 package src.application;
 
 public class Application {
+    public enum Status {
+        REJECTED,
+        DEFERRED,
+        PENDING,
+        WAITLISTED,
+        ACCEPTED
+    }
+
+    private int id;
+    private int studentId;
+    private int postSecondaryId;
+    private int programId;
+    private Status status;
+
+    public Application(int id, int studentId, int postSecondaryId, int programId, Status status) {
+        this.id = id;
+        this.studentId = studentId;
+        this.postSecondaryId = postSecondaryId;
+        this.programId = programId;
+        this.status = status;
+    }
+
+    void setStatus(Status newStatus) {
+        status = newStatus;
+    }
+
+    Status getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return 
+            id + '\n' +
+            studentId + '\n' + 
+            postSecondaryId + '\n' +
+            programId + '\n' +
+            status.name();
+    }
 }
