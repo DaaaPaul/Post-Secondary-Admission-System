4ac37697535ff1b4597a1e55931b0726187da1b3
songlinmumu <song.lin@snaplii.com>
2026-06-08 09:46:33 -0400

    Completion of PostSecondary class

--- src/postSecondary/PostSecondary.java
+++ src/postSecondary/PostSecondary.java
@@ -10,7 +10,6 @@ public abstract class PostSecondary {
     private String name;
 
     private ArrayList<Program> programs;
-    private ArrayList<Student> students;
     private ArrayList<Application> applications;
 
     public void setName(String name) {
@@ -21,13 +20,13 @@ public abstract class PostSecondary {
         this.id = id;
         this.name = name;
 
-        programs = new ArrayList<Program>();
-        students = new ArrayList<Student>();
-        applications = new ArrayList<Application>();
+        programs = new ArrayList<>();
+        applications = new ArrayList<>();
     }
 
     public void addProgram(Program p) {
         programs.add(p);
+        sortProgramsByBenchmark();
     }
 
     public boolean removeProgram(int id) {
@@ -50,5 +49,79 @@ public abstract class PostSecondary {
             }
         }
         return null;
+    }
+
+    public ArrayList<Program> searchProgramsByBenchmark(double avg) {
+        ArrayList<Program> ret = new ArrayList<>();
+        int i = 0;
+        int j = programs.size() - 1;
+
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (programs.get(m).getRequiredAverage() == avg) {
+                ret.add(programs.get(m));
+
+                i = m - 1;
+                while (i >= 0 && programs.get(i).getRequiredAverage() == avg) {
+                    ret.add(programs.get(i--));
+                }
+
+                j = m + 1;
+                while (j < programs.size() && programs.get(j).getRequiredAverage() == avg) {
+                    ret.add(programs.get(j++));
+                }
+
+                return ret;
+
+            } else if (programs.get(m).getRequiredAverage() > avg) {
+                j = m - 1;
+            } else {
+                i = m + 1;
+            }
+        }
+
+        return ret;
+    }
+
+    public void sortProgramsByBenchmark() {
+        for (int i = 0; i < programs.size() - 1; i++) {
+            int k = i;
+            for (int j = i + 1; j < programs.size(); j++) {
+                if (programs.get(j).getRequiredAverage() < programs.get(k).getRequiredAverage()) {
+                    k = j;
+                }
+            }
+            if (k != i) {
+                Program temp = programs.get(i);
+                programs.set(i, programs.get(k));
+                programs.set(k, temp);
+            }
+        }
+    }
+
+    public void addApplication(Application a) {
+        applications.add(a);
+    }
+
+    public boolean removeApplication(int id) {
+        return applications.removeIf(a -> a.compareId(id) == 0);
+    }
+
+    public abstract boolean evaluateApplication(Application a);
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Institution ID: "); sb.append(id); sb.append("\n");
+        sb.append("Institution Name: "); sb.append(name); sb.append("\n");
+
+        for (Program p : programs) {
+            sb.append("\n");
+            sb.append(p.toString());
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 }
