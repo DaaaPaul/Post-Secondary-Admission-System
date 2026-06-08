b4ac6e5455045ff1e3279e68d22ec7c093d1b8c7
songlinmumu <song.lin@snaplii.com>
2026-06-08 10:00:57 -0400

    Implementing College and University classes

--- src/postSecondary/College.java
+++ src/postSecondary/College.java
@@ -1,4 +1,40 @@
 package src.postSecondary;
 
-public class College {
+import src.application.*;
+
+public class College extends PostSecondary {
+
+    private boolean offersApprenticeship;
+    private double graduateEmploymentRate;
+
+    public boolean isOffersApprenticeship() {
+        return offersApprenticeship;
+    }
+
+    public double getGraduateEmploymentRate() {
+        return graduateEmploymentRate;
+    }
+
+    public void setOffersApprenticeship(boolean offersApprenticeship) {
+        this.offersApprenticeship = offersApprenticeship;
+    }
+
+    public void setGraduateEmploymentRate(double graduateEmploymentRate) {
+        this.graduateEmploymentRate = graduateEmploymentRate;
+    }
+
+    public College(int id, String name, boolean offersApprenticeship, double graduateEmploymentRate) {
+        super(id, name);
+        this.offersApprenticeship = offersApprenticeship;
+        this.graduateEmploymentRate = graduateEmploymentRate;
+    }
+
+    public boolean evaluateApplication(Application a) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Institution Type: College\n" + super.toString();
+    }
 }
--- src/postSecondary/University.java
+++ src/postSecondary/University.java
@@ -1,4 +1,24 @@
 package src.postSecondary;
 
-public class University {
+import src.application.*;
+
+public class University extends PostSecondary {
+
+    private int annualResearchFunding;
+    private int qsRanking;
+
+    public University(int id, String name, int annualResearchFunding, int qsRanking) {
+        super(id, name);
+        this.annualResearchFunding = annualResearchFunding;
+        this.qsRanking = qsRanking;
+    }
+
+    public boolean evaluateApplication(Application a) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Institution Type: University\n" + super.toString();
+    }
 }
